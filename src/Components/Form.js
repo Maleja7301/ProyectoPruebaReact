94e53d98034d7fb2c29247af13c2e952a8937275
Practica Aleja <maleja7301@gmail.com>
2023-05-27 14:36:44 -0500

    first commit

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -5,87 +5,176 @@ import {
   SafeAreaView,
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  FlatList,
+  Alert
 } from 'react-native';
+
 import TextInputCustom from './TextInputCustom';
 import {useState} from 'react';
 
 const Form = () => {
+  const initState = '';
   const [modalShow, setModalShow] = useState(false);
-  const [paciente, setPaciente] = useState('');
-  const [propietario, setPropietario] = useState('');
-  const [email, setEmail] = useState('');
-  const [telefono, setTelefono] = useState('');
-  const [sintomas, setSintomas] = useState('');
+  const [paciente, setPaciente] = useState(initState);
+  const [propietario, setPropietario] = useState(initState);
+  const [email, setEmail] = useState(initState);
+  const [sintomas, setSintomas] = useState(initState);
+  const [keyItem, setKeyItem] = useState(initState);
+  const [lista, setLista] = useState([]);
 
-  console.log(propietario);
+  const press = () => {
+    if ([paciente, propietario, email, sintomas].includes('')) {
+      return Alert.alert('Advertencia', 'Todos los campos son obligatorios');
+    }
+
+    let exist = lista.filter(e => {
+      return e.key === keyItem;
+    });
+    if (exist.length === 0) {
+      let newObj = {
+        key: Date.now(),
+        paciente,
+        propietario,
+        email,
+        sintomas
+      };
+      setLista([...lista, newObj]);
+    } else {
+      let list = lista.map(e => {
+        if (e.key === keyItem) {
+          e.paciente = paciente;
+          e.propietario = propietario;
+          e.email = email;
+          e.sintomas = sintomas;
+        }
+        return e;
+      });
+      setLista(list);
+    }
+    limpiarDatos();
+    setModalShow(false);
+  };
+
+  const limpiarDatos = () => {
+    setPropietario(initState);
+    setEmail(initState);
+    setSintomas(initState);
+    setPaciente(initState);
+    setKeyItem(initState);
+  };
+
+  const editPress = item => {
+    setPropietario(item.propietario);
+    setEmail(item.email);
+    setSintomas(item.sintomas);
+    setPaciente(item.propietario);
+    setKeyItem(item.key);
+    setModalShow(true);
+  };
+
+  const deletePress = item => {
+    let newList = lista.filter(e => {
+      return e.key !== item.key;
+    });
+    setLista(newList);
+  };
+
+  console.log(lista);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <Modal animationType="slide" transparent={true} visible={modalShow}>
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
             <Pressable
-              style={[styles.button, styles.buttonOpen]}
-              onPress={() => setModalShow(false)}>
-              <Text style={styles.textStyle}>Cancelar</Text>
+              style={styles.btnclose}
+              onPress={() => {
+                limpiarDatos();
+                setModalShow(false);
+              }}>
+              <Text style={styles.textStyle}>X</Text>
             </Pressable>
-
             <TextInputCustom
               label={'Nombre paciente'}
               keyboardType={'default'}
               placeholder={'Nombre paciente'}
-              onChangeText={setPaciente}
               value={paciente}
+              onChangeText={setPaciente}
             />
             <TextInputCustom
+              value={propietario}
+              onChangeText={setPropietario}
               label={'Nombre Propietario'}
               placeholder={'Nombre Propietario'}
-              onChangeText={setPropietario}
-              value={propietario}
             />
             <TextInputCustom
               label={'Email'}
+              value={email}
+              onChangeText={setEmail}
+              inputMode={'email'}
               keyboardType={'email-address'}
               placeholder={'Email'}
-              onChangeText={setEmail}
-              value={email}
             />
             <TextInputCustom
-              label={'Telefono'}
-              keyboardType={'numeric'}
-              placeholder={'Telefono'}
-              onChangeText={setTelefono}
-              maxLegth={10}
-              value={telefono}
-            />
-            <TextInputCustom
-              label={'Si­ntomas'}
-              placeholder={'Si­ntomas'}
-              multiline={true}
-              numberOfLines={3}
-              onChangeText={setSintomas}
               value={sintomas}
+              onChangeText={setSintomas}
+              label={'SÃ­ntomas'}
+              multiline={true}
+              placeholder={'SÃ­ntomas'}
             />
             <Pressable
               style={[styles.button, styles.buttonClose]}
-              onPress={() => {}}>
+              onPress={() => press()}>
               <Text style={styles.textStyle}>Enviar informacion</Text>
             </Pressable>
           </View>
         </View>
       </Modal>
-
       <Pressable
         style={[styles.button, styles.buttonOpen]}
         onPress={() => setModalShow(true)}>
         <Text style={styles.textStyle}>Registrar cita</Text>
       </Pressable>
+      <View>
+        {lista.length === 0 && <Text>No hay citas</Text>}
+        <FlatList
+          data={lista}
+          renderItem={({item}) => (
+            <View>
+              <View>
+                <Text style={styles.item}>
+                  {'Nombre paciente: ' + item.paciente}
+                </Text>
+                <Text style={styles.item}>
+                  {'Propietaro: ' + item.propietario}
+                </Text>
+                <Text style={styles.item}>{'Email: ' + item.email}</Text>
+                <Text style={styles.item}>{'Sintomas: ' + item.sintomas}</Text>
+              </View>
+              <View style={styles.contentIem}>
+                <Pressable
+                  style={[styles.button, styles.buttonDelete]}
+                  onPress={() => editPress(item)}>
+                  <Text style={styles.textStyle}>Editar</Text>
+                </Pressable>
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => deletePress(item)}>
+                  <Text style={styles.textStyle}>Eliminar</Text>
+                </Pressable>
+              </View>
+            </View>
+          )}
+        />
+      </View>
     </SafeAreaView>
   );
 };
-
 const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: '5%'
+  },
   centeredView: {
     flex: 1,
     justifyContent: 'center',
@@ -121,10 +210,31 @@ const styles = StyleSheet.create({
   buttonClose: {
     backgroundColor: '#2196F3'
   },
+  buttonDelete: {
+    backgroundColor: '#f32140',
+    marginRight: 10
+  },
   textStyle: {
     color: 'white',
     fontWeight: 'bold',
     textAlign: 'center'
+  },
+  contentIem: {
+    flexDirection: 'row',
+    marginVertical: 10
+  },
+  item: {
+    marginHorizontal: 5,
+    fontSize: 15
+  },
+  btnclose: {
+    borderRadius: 20,
+    padding: 10,
+    position: 'absolute',
+    right: 5,
+    top: 2,
+    color: '#0a0a0a',
+    backgroundColor: 'rgba(122,122,122,0.65)'
   }
 });
 
